b4f0aaa7fc66f4495834c36a4cbc20a090307ce9
Rick Conrad <richardjconrad@gmail.com>
2018-08-28 11:27:22 -0400

    Remove unused code from Java code brought over from previous BibleHead.

--- android/app/src/main/java/org/gospelcoding/biblehead/Verse.java
+++ android/app/src/main/java/org/gospelcoding/biblehead/Verse.java
@@ -1,6 +1,5 @@
 package org.gospelcoding.biblehead;
 
-import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
@@ -11,7 +10,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -41,82 +39,6 @@ public class Verse {
     Verse(){
         learned = false;
         lastReview = new Date();
-        successfulReviews = 0;
-    }
-
-    public void update(String text, String bibleBook, int bibleBookNumber, int chapterStart, int verseStart){
-        update(text, bibleBook, bibleBookNumber, chapterStart, chapterStart, verseStart, verseStart);
-    }
-
-    public void update(String text, String bibleBook, int bibleBookNumber, int chapterStart, int chapterEnd, int verseStart, int verseEnd){
-        this.text = text;
-        this.bibleBook = bibleBook;
-        this.bibleBookNumber = bibleBookNumber;
-        this.chapterStart = chapterStart;
-        this.chapterEnd = chapterEnd;
-        this.verseStart = verseStart;
-        this.verseEnd = verseEnd;
-    }
-
-    public boolean comesAfter(Verse otherVerse){
-        if (bibleBookNumber != otherVerse.bibleBookNumber)
-            return bibleBookNumber > otherVerse.bibleBookNumber;
-
-        if (chapterStart != otherVerse.chapterStart)
-            return chapterStart > otherVerse.chapterStart;
-
-        return verseStart > otherVerse.verseStart;
-
-    }
-
-//    // Does not validate that the book contains the chapters/verses indicated
-//    public boolean isValid(){
-//        validationErrors = new ArrayList();
-//
-//        if (text == null || text.length() == 0)
-//            validationErrors.add(R.string.verse_text_not_blank);
-//
-//        if (chapterEnd < chapterStart || (chapterEnd == chapterStart && verseEnd < verseStart))
-//            validationErrors.add(R.string.end_verse_after_start_verse);
-//
-//        if (chapterStart <= 0 || chapterEnd <= 0 || chapterStart > 150 || chapterEnd > 150)
-//            validationErrors.add(R.string.valid_chapter_numbers);
-//
-//        if (verseStart <= 0 || verseEnd <= 0 || verseStart > 200 || verseEnd > 200)
-//            validationErrors.add(R.string.valid_verse_numbers);
-//
-//        return validationErrors.size() == 0;
-//    }
-
-//    public String getReference(){
-//        String ref = bibleBook
-//                + ' ' + String.valueOf(chapterStart)
-//                + ':' + String.valueOf(verseStart);
-//
-//        if(chapterEnd != chapterStart){
-//            ref += '-' + String.valueOf(chapterEnd)
-//                       + ':' + String.valueOf(verseEnd);
-//        }
-//        else if(verseEnd != verseStart){
-//            ref += '-' + String.valueOf(verseEnd);
-//        }
-//
-//        return ref;
-//    }
-
-    public int getProgress(){
-        double progress = 100 - (1000 / (successfulReviews + 10.0));
-        return (int) Math.round(progress);
-    }
-
-    public void markReviewed(boolean success){
-        lastReview = new Date();
-        if (success)
-            ++successfulReviews;
-    }
-
-    public void toggleLearned(boolean learned){
-        this.learned = learned;
         successfulReviews = 0;
     }
 
--- android/app/src/main/java/org/gospelcoding/biblehead/VerseDao.java
+++ android/app/src/main/java/org/gospelcoding/biblehead/VerseDao.java
@@ -1,33 +1,13 @@
 package org.gospelcoding.biblehead;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
-import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
 
 import java.util.List;
 
 @Dao
 public interface VerseDao {
-    @Query("SELECT * FROM verse WHERE id=:id")
-    Verse find(int id);
-
-    @Query("SELECT * FROM verse WHERE id IN (:ids)")
-    List<Verse> getById(int... ids);
-
     @Query("SELECT * From verse ORDER BY bibleBookNumber, chapterStart, verseStart")
     List<Verse> getAll();
 
-    @Query("SELECT * FROM verse WHERE learned ORDER BY (3*(julianday('now') - julianday(lastReview)) - successfulReviews) DESC LIMIT :numberOfVerses")
-    List<Verse> getVersesForReview(int numberOfVerses);
-
-    @Insert
-    long insert(Verse verse);
-
-    @Update
-    void update(Verse verse);
-
-    @Delete
-    void deleteVerses(Verse... verses);
 }
