3c1eaea6cb8a708fb57158beb13fd00b6fb8be92
Rick Conrad <richardjconrad@gmail.com>
2020-07-21 11:31:01 -0400

    Navigation

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import BibleHeadApp from './BibleHeadApp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => BibleHeadApp);
