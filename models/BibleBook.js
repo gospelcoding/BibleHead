6478bf6f304494177127a93418fedc675a3d3366
Rick Conrad <richardjconrad@gmail.com>
2020-07-21 09:41:00 -0400

    Reboot

--- models/BibleBook.js
+++
@@ -1,7 +0,0 @@
-import I18n from "../i18n/i18n";
-
-export default class BibleBook {
-  static books(lang) {
-    return lang ? I18n.t("bibleBooks", { locale: lang }) : I18n.t("bibleBooks");
-  }
-}
