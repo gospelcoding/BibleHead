31d3588558eb0f417ff4e2b58274b9929f951de7
Rick Conrad <richardjconrad@gmail.com>
2020-07-22 13:13:27 -0400

    Put code in src folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import BibleHeadApp from './BibleHeadApp';
+import BibleHeadApp from './src/BibleHeadApp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => BibleHeadApp);
