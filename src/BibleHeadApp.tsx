e6a671d0f87f92abb8ac97fdaef4422bd8b50aaa
Rick Conrad <richardjconrad@gmail.com>
2020-08-17 16:35:58 -0400

    StatusBar

--- src/BibleHeadApp.tsx
+++ src/BibleHeadApp.tsx
@@ -4,6 +4,10 @@ import {stringsForDeviceLocale} from './i18n/i18nRN';
 import {I18nContext, useT} from './i18n/i18nReact';
 import BHState from './BHState';
 import LearningStack from './learning/LearningStack';
+import {Platform, StatusBar} from 'react-native';
+import ThemeColors from './util/ThemeColors';
+
+const isAndroid = Platform.OS == 'android';
 
 export default function BibleHeadApp() {
   const [deviceStrings] = useState(stringsForDeviceLocale());
@@ -12,6 +16,10 @@ export default function BibleHeadApp() {
     <BHState>
       <I18nContext.Provider value={deviceStrings}>
         <NavigationContainer>
+          <StatusBar
+            backgroundColor={ThemeColors.darkBlue}
+            barStyle="dark-content"
+          />
           <LearningStack />
         </NavigationContainer>
       </I18nContext.Provider>
